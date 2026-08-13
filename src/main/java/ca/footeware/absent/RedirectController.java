3e2ca86f922cb1de8fbd1958a35946e50a47c55c
Craig Foote <CraigFoote@gmail.com>
2026-08-12 22:11:58 -0400

    Support basic auth using headers

--- src/main/java/ca/footeware/absent/RedirectController.java
+++ src/main/java/ca/footeware/absent/RedirectController.java
@@ -4,33 +4,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
+@RequestMapping("/")
 public class RedirectController {
 
 	/**
-	 * A request to '/someName' will return a '301 Moved Permanently' to '/redirect/someName'.
+	 * A request to '/name/someName' will return a '301 Moved Permanently' to '/name/redirect/someName'.
 	 * 
 	 * @param name {@link String}
 	 * @return {@link RedirectView}
 	 */
-	@GetMapping(value = "/{name}")
+	@GetMapping(value = "/name/{name}")
 	public RedirectView getName(@PathVariable String name) {
-		String newUrl = "/redirect/" + name;
+		String newUrl = "/name/redirect/" + name;
 		RedirectView redirectView = new RedirectView(newUrl);
 		redirectView.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
 		return redirectView;
 	}
 
 	/**
-	 * A request to '/redirect/someName' will return a '200 OK' with content 'someName'.
+	 * A request to '/name/redirect/someName' will return a '200 OK' with content 'someName'.
 	 * 
 	 * @param name {@link String}
 	 * @return {@link ResponseEntity}
 	 */
-	@GetMapping(value = "/redirect/{name}")
+	@GetMapping(value = "/name/redirect/{name}")
 	public ResponseEntity<String> getRedirectedName(@PathVariable String name) {
 		return ResponseEntity.ok(name);
 	}
